5c3ad32ca51af4156f144cd05cfef92840b2309d
Bru <brupallasvargues@gmail.com>
2026-04-11 12:50:44 +0200

    groups screen

--- core/views.py
+++ core/views.py
@@ -7,5 +7,17 @@ def dashboard(request):
         "pages/dashboard.html",
         {
             "meta_title": "Dashboard",
+            "active_page": "dashboard",
+        },
+    )
+
+
+def groups(request):
+    return render(
+        request,
+        "pages/groups.html",
+        {
+            "meta_title": "Groups",
+            "active_page": "groups",
         },
     )
