94f9f9ef7b3a748bda38ecd6023180ff60dc4317
Aleix Bertran <aleixbertranandreu@gmail.com>
2026-04-11 17:48:03 +0200

    backend: implement voting logic and plan creation endpoints

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("archive/", views.archive, name="archive"),
     path("start-new-plan/", views.start_new_plan, name="start_new_plan"),
     path("groups/", views.groups, name="groups"),
+    path("vote/<int:group_id>/", views.vote, name="vote"),
+    path("api/vote/<int:plan_id>/", views.api_submit_vote, name="api_submit_vote"),
 ]
